5570a53d8db90319be51fa80a2550c5222da6f1e
Price <efaris@uoregon.edu>
2021-10-09 14:37:22 -0700

    Fixed

--- Labs/Lab_1/string_parser.c
+++ Labs/Lab_1/string_parser.c
@@ -25,6 +25,38 @@ int count_token (char* buf, const char* delim)
 	*			c.	account NULL for the last token
 	*	#3. return the number of token (note not number of delimeter)
 	*/
+
+	// To see if NULL String
+	if (buf == NULL){
+		return EXIT_FAILURE;
+	}
+
+	int count = 0;
+
+	// For some reason I have to make a copy of buf. It "Aborted (core dumped) without the strdup, I am not sure why."
+	char* copy = strdup(buf);
+
+	char* token;
+	char* saved;
+	char* ptr;
+
+	strtok_r(copy, "\n", &saved);
+
+	for(ptr = copy;; ptr = NULL){
+		token = strtok_r(ptr, delim, &saved);
+		if(token != NULL){
+			count++;
+		}
+		else{
+			break;
+		}
+	}
+	
+
+	// Mem leaks cus of strdup
+	free(copy);
+	return count + 1;
+
 }
 
 command_line str_filler (char* buf, const char* delim)
@@ -41,6 +73,36 @@ command_line str_filler (char* buf, const char* delim)
 	*			fill command_list array with tokens, and fill last spot with NULL.
 	*	#6. return the variable.
 	*/
+
+	command_line answer;
+	
+	int tokens = count_token(buf, delim);
+	answer.num_token = tokens;
+
+	answer.command_list = (char**)malloc(sizeof(char*) * answer.num_token);
+
+	char* saved;
+	char* ptr = buf;
+
+	strtok_r(buf, "\n", &saved);
+
+	int i = 0;
+	for(ptr;; ptr = NULL, i++){
+		char* tok = strtok_r(ptr,delim,&saved);
+		if(tok != NULL){
+			// For some reason I have to make a copy of buf. It "Aborted (core dumped) without the strdup, I am not sure why."
+			answer.command_list[i] = strdup(tok);
+		}
+		else{
+			break;
+		}
+	}
+
+	answer.command_list[(answer.num_token) - 1] = NULL;
+
+	
+	
+	return answer;
 }
 
 
@@ -50,4 +112,10 @@ void free_command_line(command_line* command)
 	/*
 	*	#1.	free the array base num_token
 	*/
+
+	for(int i = 0; i < command->num_token; i++){
+		free(command->command_list[i]);
+	}
+
+	free(command->command_list);
 }
