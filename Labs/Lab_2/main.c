9cfc10168e4b0a6fc2d320a3c2fd462d402e7a49
Price <efaris@uoregon.edu>
2021-10-10 15:52:17 -0700

    Testing

---
+++ Labs/Lab_2/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "command.h"
+
+#define _GNU_SOURCE
+
+int main(){
+    char str[100];
+
+    printf(">>>");
+    str = gets(str)
+
+    while (str != "lfcat")
+    {
+        printf("Error: Unrecognized command!\n")
+
+        printf(">>>");
+
+    }
+        
+    
+}
