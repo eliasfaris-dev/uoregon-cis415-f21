b394d6295f68a83c97ecc29584e190a64b2a9b31
Price <efaris@uoregon.edu>
2021-10-10 17:21:43 -0700

    Testing

--- Labs/Lab_2/main.c
+++ Labs/Lab_2/main.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[]){
     while(1){
         printf(">>> ");
         fgets(str, 1048, stdin);
-        if((strncmp(str, "lfcat", 6)) == 1){
+        if((strncmp(str, "lfcat", 5)) == 1){
             //break;
             return 0;
         }
