84d7aa8411c7ac747427b2d74676ee2361b3e696
Price <efaris@uoregon.edu>
2021-10-10 17:30:19 -0700

    Testing

--- Labs/Lab_2/main.c
+++ Labs/Lab_2/main.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[]){
 
     printf(">>> ");
 
-    while(fgets(str, 1048, stdin) != "lfcat"){
+    while(fgets(str, 1048, stdin) != "lfcat\n"){
         printf("Error: Unrecognized command!\n");
         printf(">>> ");
     }
