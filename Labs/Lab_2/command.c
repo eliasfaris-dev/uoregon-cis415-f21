cc48b3191932a12d66bf4b463f900b47f9a8eac3
Price <efaris@uoregon.edu>
2021-10-11 00:27:17 -0700

    Testing

--- Labs/Lab_2/command.c
+++ Labs/Lab_2/command.c
@@ -3,16 +3,28 @@
 #include <string.h>
 #include "command.h"
 #include <unistd.h>
+#include <sys/types.h>
+#include <dirent.h>
 
 
 void lfcat(char* buf, long size){
 
-    
+    DIR* pdir;
     char* ptr;
+
+    struct dirent *dirp;
+
     printf("<<In lfcat(): Step-01: Function called>>\n");
     if ((buf = (char *)malloc((size_t)size)) != NULL){
         ptr = getcwd(buf, (size_t) size);
         printf("CWD: %s\n", buf);
+        pdir = opendir(buf);
+        
+        while((dirp = readdir(pdir)) != NULL){
+            printf("%s\n", dirp->d_name);
+        }
+
+        closedir(pdir);
     }
 
 
