29d2f237c3749d303fd927a3da936bf4d4026c1c
Price <efaris@uoregon.edu>
2021-10-11 20:13:25 -0700

    Testing

--- Labs/Lab_2/command.c
+++ Labs/Lab_2/command.c
@@ -27,7 +27,7 @@ void lfcat(char* buf, long size){
         while((dirp = readdir(pdir)) != NULL){
             printf("File: %s\n", dirp->d_name);
             write(1, dirp->d_name, strlen(dirp->d_name));
-
+            
         }
 
         closedir(pdir);
