46dbf9a9f96f724dc181b99d541644e3b7dc79d2
Price <efaris@uoregon.edu>
2021-10-11 21:06:08 -0700

    Testing

--- Labs/Lab_2/command.c
+++ Labs/Lab_2/command.c
@@ -30,10 +30,11 @@ void lfcat(char* buf, long size){
         while((dirp = readdir(pdir)) != NULL){
             printf("File: %s\n", dirp->d_name);
             while((nread = getline(&line, &len, fp)) != -1){
-                write(nread, line, len);
+                fprintf(fp, "%s\n", line);
             }
+
             //write(1, dirp->d_name, strlen(dirp->d_name));
-            printf("--------------------------------------------------------------------------------");
+            printf("--------------------------------------------------------------------------------\n");
         }
 
         closedir(pdir);
