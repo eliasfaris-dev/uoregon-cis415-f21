4e40131872ceee67d253d135f9ee471bd7382c87
Price <efaris@uoregon.edu>
2021-10-11 20:45:28 -0700

    Testing

--- Labs/Lab_2/command.c
+++ Labs/Lab_2/command.c
@@ -12,6 +12,9 @@ void lfcat(char* buf, long size){
     DIR* pdir;
     char* ptr;
     FILE* fp;
+    ssize-t nread;
+    size_t len = 0;
+    char* line = NULL;
 
     struct dirent *dirp;
 
@@ -19,15 +22,18 @@ void lfcat(char* buf, long size){
     if ((buf = (char *)malloc((size_t)size)) != NULL){
         ptr = getcwd(buf, (size_t) size);
         printf("<<In lfcat(): Step-02: Listing all files in current dir.\n");
-        printf("CWD: %s\n", buf);
+        //printf("CWD: %s\n", buf);
         pdir = opendir(buf);
         
         fp = freopen("output.txt", "w+", stdout);
 
         while((dirp = readdir(pdir)) != NULL){
             printf("File: %s\n", dirp->d_name);
-            write(1, dirp->d_name, strlen(dirp->d_name));
-            
+            while((nread = getline(&line, &len, fp)) != -1){
+                write(line, nread, 1);
+            }
+            //write(1, dirp->d_name, strlen(dirp->d_name));
+            printf("--------------------------------------------------------------------------------")
         }
 
         closedir(pdir);
