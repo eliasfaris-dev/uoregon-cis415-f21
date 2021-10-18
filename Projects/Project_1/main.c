a22a1980b8e2160217ca2b5a87d9c738fc9878a2
Price <efaris@uoregon.edu>
2021-10-18 00:02:08 -0700

    hello

--- Projects/Project_1/main.c
+++ Projects/Project_1/main.c
@@ -323,7 +323,6 @@ int vaildate(char** tokens){
 
 void call(char** tokens){
 	printf("here: %s\n", *tokens);
-	printf("here1: %s\n", tokens[1]);
 	char* element = tokens[0];
 	if(strcmp(element, "ls") == 0){
 		listDir();
