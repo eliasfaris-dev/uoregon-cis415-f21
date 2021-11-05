bca8aab5d9e6f4b0e7b1dc135d122d6491bdc794
Price <efaris@uoregon.edu>
2021-11-04 17:11:45 -0700

    Testing

--- Labs/Lab_5/part1.c
+++ Labs/Lab_5/part1.c
@@ -31,23 +31,23 @@ void fileMode(char **argv){
 		for(int i = 0; commands[i] != NULL; i++){
 			second = str_filler(commands[i], " ");
 			tokens = second.command_list;
-			if(validate(tokens)){
-				if(strcmp(tokens[0], "exit") == 0){	
-					fclose(fp);
-					free(buf);
-					free_commands(commands);
-					free_commands(tokens);
-					memset(&second, 0, 0);
-					free_commands(commands);
-					memset(&first, 0, 0);
-					return;
-				}
-				else{
-					call(tokens);
-					free_commands(tokens);
-					memset(&second, 0, 0);
-				}
+			
+			if(strcmp(tokens[0], "exit") == 0){	
+				fclose(fp);
+				free(buf);
+				free_commands(commands);
+				free_commands(tokens);
+				memset(&second, 0, 0);
+				free_commands(commands);
+				memset(&first, 0, 0);
+				return;
 			}
+			else{
+			    //call(tokens);
+				free_commands(tokens);
+				memset(&second, 0, 0);
+			}
+			
 			else{
 				free_commands(tokens);
 				memset(&second, 0, 0);
@@ -61,129 +61,6 @@ void fileMode(char **argv){
 	return;
 }
 
-int validate(char** tokens){
-	int answer = 0;
-	int counter;
-	for(int i = 0; tokens[i] != NULL; i++){
-		counter++;
-	}
-	if(strcmp(tokens[0], "ls") == 0){
-		if(counter == 1){
-			answer =  1;
-		}
-		else{
-			// Figure out a way to implement where this is printing in a file or on console depending on file mode or not
-			printf("Error! Unsupported parameters for command: %s\n", tokens[0]);
-			answer = 0;
-		}
-	}
-	else if(strcmp(tokens[0], "pwd") == 0){
-		if(counter == 1){
-			answer = 1;
-		}
-		else{
-			printf("Error! Unsupported parameters for command: %s\n", tokens[0]);
-			answer = 0;
-		}
-	}
-	else if(strcmp(tokens[0], "mkdir") == 0){
-		if(counter == 2){
-			answer = 1;
-		}
-		else{
-			printf("Error! Unsupported parameters for command: %s\n", tokens[0]);
-			answer = 0;
-		}
-	}
-	else if(strcmp(tokens[0], "cd") == 0){
-		if(counter == 2){
-			answer = 1;
-		}
-		else{
-			printf("Error! Unsupported parameters for command: %s\n", tokens[0]);
-			answer = 0;
-		}
-	}
-	else if(strcmp(tokens[0], "cp") == 0){
-		if(counter == 3){
-			answer = 1;
-		}
-		else{
-			printf("Error! Unsupported parameters for command: %s\n", tokens[0]);
-			answer = 0;
-		}
-	}
-	else if(strcmp(tokens[0], "mv") == 0){
-		if(counter == 3){
-			answer = 1;
-		}
-		else{
-			printf("Error! Unsupported parameters for command: %s\n", tokens[0]);
-			answer = 0;
-		}
-	}
-	else if(strcmp(tokens[0], "rm") == 0){
-		if(counter == 2){
-			answer = 1;
-		}
-		else{
-			printf("Error! Unsupported parameters for command: %s\n", tokens[0]);
-			answer = 0;
-		}
-	}
-	else if(strcmp(tokens[0], "cat") == 0){
-		if(counter == 2){
-			answer = 1;
-		}
-		else{
-			printf("Error! Unsupported parameters for command: %s\n", tokens[0]);
-			answer = 0;
-		}
-	}
-	// Check last 2 if/else checks
-	else if(strcmp(tokens[0], "exit") == 0){
-		if(counter == 1){
-			answer = 1;
-		}
-		else{
-			printf("Error! Unsupported parameters for command: %s\n", tokens[0]);
-			answer = 0;
-		}
-	}
-	else{
-		printf("Error! Unrecognized command: %s\n", tokens[0]);
-		answer = 0;
-	}
-	return answer;
-}
-
-void call(char** tokens){
-	char* element = tokens[0];
-	if(strcmp(element, "ls") == 0){
-		listDir();
-	}
-	else if(strcmp(element, "pwd") == 0){
-		showCurrentDir();
-	}
-	else if(strcmp(element, "mkdir") == 0){
-		makeDir(tokens[1]);
-	}
-	else if(strcmp(element, "cd") == 0){
-		changeDir(tokens[1]);
-	}
-	else if(strcmp(element, "cp") == 0){
-		copyFile(tokens[1], tokens[2]);
-	}
-	else if(strcmp(element, "mv") == 0){
-		moveFile(tokens[1],tokens[2]);
-	}
-	else if(strcmp(element, "rm") == 0){
-		deleteFile(tokens[1]);
-	}
-	else if(strcmp(element, "cat") == 0){
-		displayFile(tokens[1]);
-	}
-}
 
 int processes(char*argv[]){
     //FILE* fd = freopen("log.txt", "w", stdout);
