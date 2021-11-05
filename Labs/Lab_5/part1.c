702d1bc166ac6ac6f7312f4794f86189c350dc54
Price <efaris@uoregon.edu>
2021-11-04 19:30:51 -0700

    Testing

--- Labs/Lab_5/part1.c
+++ Labs/Lab_5/part1.c
@@ -5,116 +5,7 @@
 #include "string_parser.h"
 
 int main(int argc, char*argv[]){
-
-    FILE* fw = stdout;  // output file
-    FILE* fr = stdin;   // input file
-    char *linebuf = NULL;   // read line buffer
-    size_t linecap = 0;
-    char write_buf[BUFSIZ]; // output write buffer
-    command_line large_token_buffer;
-	command_line small_token_buffer;
-    int parent = getpid();
-    pid_t pid_new;
-
-    if ((argc == 3) && (strcmp(argv[1],"-f") == 0))
-	{   // file mode
-        //fw = freopen("output.txt", "w", stdout);
-        fr = freopen(argv[2], "r", stdin);
-	}
-	else
-	{   // error handling
-		strcpy(write_buf, "Usage ./pseudo-shell -f <filename>\n");
-		write(1, write_buf, strlen(write_buf));
-		goto cleanup;
-	}
-	strcpy(write_buf, ">>> ");
-    write(1, write_buf, strlen(write_buf));     // output initial prompt
-
-    //loop until interactive exit or input command file ends
-	while (getline (&linebuf, &linecap, fr) != -1)
-	{
-	    write(1, linebuf, strlen(linebuf));
-		//tokenize line buffer
-		//large token is seperated by ";"
-		large_token_buffer = str_filler (linebuf, ";");
-
-		//iterate through each large token
-		for (int i = 0; large_token_buffer.command_list[i] != NULL; i++)
-		{
-			//tokenize large buffer
-			//smaller token is seperated by " "(space bar)
-			small_token_buffer = str_filler (large_token_buffer.command_list[i], " ");
-
-            pid_new = fork();
-            if(pid_new < 0){
-                printf("Unable to declare child process");
-                //free(pid_ary);
-            }
-
-            if (parent != getpid()) {
-
-                if(execvp(small_token_buffer.command_list[0], small_token_buffer.command_list) == -1){
-                    printf("New process couldn't be made\n");
-                    //free(pid_ary);
-                }
-
-                exit(0);
-            }
-
-			//free smaller tokens and reset variable
-			free_command_line(&small_token_buffer);
-			memset (&small_token_buffer, 0, 0);
-		}
-
-		//free smaller tokens and reset variable
-		free_command_line (&large_token_buffer);
-		memset (&large_token_buffer, 0, 0);
-        strcpy(write_buf, ">>> ");
-        write(1, write_buf, strlen(write_buf));
-
-	}
-
-cleanup:
-    //strcpy(write_buf, "Exiting pseudo-shell\n");
-    //write(1, write_buf, 21);
-    //fclose(fw);
-    fclose(fr);
-    free(linebuf);
-    return 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
+    /*
     char* arg_list[] = {"./iobound", "-seconds", "5", NULL};
     int num_proc = atoi(argv[1]);
     int count;
@@ -145,34 +36,65 @@ cleanup:
     free(pid_ary);
     //fclose(fd);
     return 0;
+    */
+
+
+    
+    if(argc != 2){
+        printf("Error! You have the wrong number of paramaters\n");
+    }
+    else{
+        int num_proc = atoi(argv[1]);
+        fileMode(argv, num_proc);
+    }
+
 }
 
-void script_print (pid_t* pid_ary, int size){
-    FILE* fout;
-    fout = fopen ("top_script.sh", "w");
-    fprintf(fout, "#!/bin/bash\ntop");
-    for (int i = 0; i < size; i++){
-        fprintf(fout, " -p %d", (int)(pid_ary[i]));
-    }
-    fprintf(fout, "\n");
-    fclose (fout);
+void fileMode(char **argv, int num_proc){
+	FILE* fp;
+	fp = fopen(argv[2], "r");
+	// Got from my Lab 1
+	size_t length = 128;
+	char* buf = malloc(length);
 
-    char* top_arg[] = {"gnome-terminal", "--", "bash", "top_script.sh", NULL};
-    pid_t top_pid;
+	command_line first;
+	char** tokens;
 
-    top_pid = fork();
-    {
-        if (top_pid == 0)
-        {
-            if(execvp(top_arg[0], top_arg) == -1)
-            {
-                perror ("top command: ");
+	while(getline(&buf, &length, fp) != -1){
+		first = str_filler(buf, " ");
+		tokens = first.command_list;
+		call(tokens, num_proc);
+		free_command_line(&first);
+        memset(&tokens, 0, 0);
+	}
+	free(buf);
+    fclose(fp);
+    exit(-1);
+	return;
+}
+
+void call(char** tokens, int num_proc){
+    int found = getpid();
+
+    pid_t* pid_ary = (pid_t*)malloc(sizeof(pid_t) * num_proc);
+    for(int i = 0; i < num_proc; i++){
+        pid_ary[i] = fork();
+
+        if(pid_ary[i] < 0){
+            printf("Unable to declare child process");
+            free(pid_ary); 
+        }
+
+        if(found != getpid()){
+        
+            if(execvp(tokens[0], tokens) == -1){
+                printf("New process couldn't be made\n");
+                free(pid_ary);
             }
-            exit(0);
+            exit(-1);
         }
     }
 }
-*/
 
 int count_token (char* buf, const char* delim)
 {
@@ -238,6 +160,10 @@ command_line str_filler (char* buf, const char* delim)
 
 void free_command_line(command_line* command)
 {
+	//TODO：
+	/*
+	*	#1.	free the array base num_token
+	*/
 
 	for(int i = 0; i < command->num_token; i++){
 		free(command->command_list[i]);
