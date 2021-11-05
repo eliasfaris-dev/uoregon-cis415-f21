64f93248ba4c841d584c8ca302e30802422393d1
Price <efaris@uoregon.edu>
2021-11-05 12:28:43 -0700

    test

--- Labs/Lab5/part1.c
+++ Labs/Lab5/part1.c
@@ -20,8 +20,9 @@ int main(int argc,char*argv[]){
 	command_line tokens;
 	pid_t* pid_ary = (pid_t*)malloc(sizeof(pid_t) * 50);
 
-    while(getline(buf, &length, fp) != -1){
+    while(getline(&buf, &length, fp) != -1){
 		tokens = str_filler(buf, " ");
+		token = tokens.command_list;
 		n++;
         pid_ary[n] = fork();
 
@@ -34,7 +35,7 @@ int main(int argc,char*argv[]){
 		//printf(tokens);
         if(found != getpid()){
         
-            if(execvp(tokens.command_list[0], tokens.command_list) == -1){
+            if(execvp(token[0], token) == -1){
                 printf("New process couldn't be made\n");
                 free(pid_ary);
             }
