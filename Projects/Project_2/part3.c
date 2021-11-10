6682cb07604be9219f485f68bd00fed483d6ff23
Price <efaris@uoregon.edu>
2021-11-09 22:16:38 -0800

    Test this

--- Projects/Project_2/part3.c
+++ Projects/Project_2/part3.c
@@ -18,7 +18,7 @@ int main(int argc,char*argv[]){
     
     sigset_t signal_set;
     sigemptyset(&signal_set);
-    sigaddset(&signal_set, SIGUSR1);
+    //sigaddset(&signal_set, SIGUSR1);
     sigaddset(&signal_set, SIGCONT);
     sigaddset(&signal_set, SIGALRM);
     sigprocmask(SIG_BLOCK, &signal_set, NULL);
