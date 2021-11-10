79c4f473ef22e8bd0cc519ccd60f91544cff2d54
Price <efaris@uoregon.edu>
2021-11-09 21:53:36 -0800

    Test this

--- Projects/Project_2/part3.c
+++ Projects/Project_2/part3.c
@@ -99,14 +99,17 @@ int main(int argc,char*argv[]){
     while(1){
         for(int i = 0; i < n; i++){
             
-            if(done_ary[i] != n){
+            if(done_ary[i] != 100){
+                printf("cont process %d\n", i);
                 kill(pid_ary[i], SIGCONT);
                 alarm(2);
                 sigwait(&signal_set, &signal);
+                printf("stop process %d\n", i);
                 kill(pid_ary[i], SIGSTOP);
             }
 
             if(waitpid(pid_ary[i]), &count, WNOHANG) != 0){
+                printf("process %d DONE\n", j);
                 done += 1;
                 done_ary[i] = 100;
             }
