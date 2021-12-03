b53993598890a435c73f42f8e836e97f63e909b2
Price <efaris@uoregon.edu>
2021-12-02 16:13:55 -0800

    Test

--- Labs/Lab_8/part2.c
+++ Labs/Lab_8/part2.c
@@ -22,14 +22,12 @@ pthread_mutex_t lock;
 int main(int argc, char** argv){
 	if(argc != 2){
 		printf("Incorrect call of function");
-		exit(EXIT_FAILURE);
 	}
 	else{
 		
 		FILE* fp = fopen(argv[1], "r");
 		if(fp == NULL){
 			printf("File not found");
-			exit(EXIT_FAILURE);
 		}
 		
 		else{
