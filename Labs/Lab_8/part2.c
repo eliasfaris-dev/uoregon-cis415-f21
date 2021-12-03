84a62269c5a78bef2298970858a006b1d1ffa07a
Price <efaris@uoregon.edu>
2021-12-02 18:16:40 -0800

    Test

--- Labs/Lab_8/part2.c
+++ Labs/Lab_8/part2.c
@@ -91,17 +91,16 @@ int main(int argc, char** argv){
 			
 			pthread_join(b_thread, NULL);
 
-			
 			for(int i = 0; i < total_acc; i++){
 				printf("%d balance:  %0.2f\n", i, the_acc[i].balance);
 			}
 
-			/*
+			
 			for(int i = 0; i < index; i++){
-				free_command_line(&tokens[i]);
+				free_command_line(tokens[i]);
 			}
 			free_command_line(&tokens);
-			*/
+			
 			fclose(fp);
 			free(buf);
 			free(the_acc);
