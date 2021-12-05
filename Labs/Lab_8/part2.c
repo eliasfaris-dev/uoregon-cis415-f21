8d742ce64df08f8b734cf925e106e9b66f77a3cf
Price <efaris@uoregon.edu>
2021-12-04 18:28:21 -0800

    Test

--- Labs/Lab_8/part2.c
+++ Labs/Lab_8/part2.c
@@ -115,8 +115,8 @@ void process_transaction(void* arg){
             for(int i = 0; i < total_acc; i++){
                 if((strcmp(tokens[j].command_list[1], the_acc[i].account_number) == 0)){
                     if(strcmp(tokens[j].command_list[2], the_acc[i].password) == 0){
-						pthread_mutex_lock(&the_acc[i].ac_lock);
-						pthread_mutex_unlock(&the_acc[i].ac_lock);
+						pthread_mutex_lock(&lock);
+						pthread_mutex_unlock(&lock);
                         break;
                     }
                 }
@@ -127,10 +127,10 @@ void process_transaction(void* arg){
 			for(int i = 0; i < total_acc; i++){
 				if((strcmp(tokens[j].command_list[1], the_acc[i].account_number) == 0)){
                     if(strcmp(tokens[j].command_list[2], the_acc[i].password) == 0){
-						pthread_mutex_lock(&the_acc[i].ac_lock);
+						pthread_mutex_lock(&lock);
                         the_acc[i].transaction_tracter += amount;
 						the_acc[i].balance += amount;
-						pthread_mutex_unlock(&the_acc[i].ac_lock);
+						pthread_mutex_unlock(&lock);
                         break;
                     }
                 }
@@ -141,10 +141,11 @@ void process_transaction(void* arg){
             for(int i = 0; i < total_acc; i++){
                 if((strcmp(tokens[j].command_list[1], the_acc[i].account_number) == 0)){
                     if(strcmp(tokens[j].command_list[2], the_acc[i].password) == 0){
-						pthread_mutex_lock(&the_acc[i].ac_lock);
+						pthread_mutex_lock(&lock);
                         the_acc[i].transaction_tracter += amount;
 						the_acc[i].balance -= amount;
-						pthread_mutex_unlock(&the_acc[i].ac_lock);
+						pthread_mutex_unlock(&lock);
+                        break;
                     }
                 }
             }
@@ -157,13 +158,11 @@ void process_transaction(void* arg){
                     if(strcmp(tokens[j].command_list[2], the_acc[i].password) == 0){
                         for(int k = 0; k < total_acc; k++){
                             if(strcmp(tokens[j].command_list[3], the_acc[k].account_number) == 0){
-								pthread_mutex_lock(&the_acc[i].ac_lock);
-								pthread_mutex_lock(&the_acc[k].ac_lock);
+								pthread_mutex_lock(&lock);
                                 the_acc[i].balance -= amount;
                                 the_acc[i].transaction_tracter += amount;
                                 the_acc[k].balance += amount;
-								pthread_mutex_unlock(&the_acc[k].ac_lock);
-								pthread_mutex_unlock(&the_acc[i].ac_lock);
+								pthread_mutex_unlock(&lock);
                                 break;
                             }
                         }
