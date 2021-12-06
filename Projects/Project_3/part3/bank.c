0b200a226415dafcf2caa1329860c21a5d31eb3e
Price <efaris@uoregon.edu>
2021-12-05 21:40:22 -0800

    Test

--- Projects/Project_3/part3/bank.c
+++ Projects/Project_3/part3/bank.c
@@ -156,10 +156,7 @@ void process_transaction(void* arg){
             for(int i = 0; i < total_acc; i++){
                 if((strcmp(tokens[j].command_list[1], the_acc[i].account_number) == 0)){
                     if(strcmp(tokens[j].command_list[2], the_acc[i].password) == 0){
-                        
-						printf("C\n");
-						
-						
+
                     }
                 }
             }
@@ -173,7 +170,6 @@ void process_transaction(void* arg){
 						pthread_mutex_lock(&the_acc[i].ac_lock);
                         the_acc[i].transaction_tracter += amount;
 						the_acc[i].balance += amount;
-						printf("D\n");
 						pthread_mutex_unlock(&the_acc[i].ac_lock);
 						pthread_mutex_lock(&lock);
 						completed++;
@@ -191,7 +187,6 @@ void process_transaction(void* arg){
 						pthread_mutex_lock(&the_acc[i].ac_lock);
                         the_acc[i].transaction_tracter += amount;
 						the_acc[i].balance -= amount;
-						printf("W\n");
 						pthread_mutex_unlock(&the_acc[i].ac_lock);
 						pthread_mutex_lock(&lock);
 						completed++;
@@ -212,7 +207,6 @@ void process_transaction(void* arg){
 								pthread_mutex_lock(&the_acc[i].ac_lock);
                                 the_acc[i].balance -= amount;
                                 the_acc[i].transaction_tracter += amount;
-								printf("T\n");
 								pthread_mutex_unlock(&the_acc[i].ac_lock);
 								pthread_mutex_lock(&the_acc[k].ac_lock);
                                 the_acc[k].balance += amount;
